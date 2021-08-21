bbf4b3f6cf2d7d1753337338ded747b55f38a9e4
olakrhoff <ola.kr@hotmail.no>
2021-08-21 11:36:11 +0200

    env

--- src/enviorment.h
+++ src/enviorment.h
@@ -1,7 +1,7 @@
 #ifndef ENVIORMENT_H
 #define ENVIORMENT_H
 
-#define ENV_PATH ".../shellLink" //This path needs to be upadeted to hold the full path to the shellLink folder
+#define ENV_PATH "kake/shellLink" //This path needs to be upadeted to hold the full path to the shellLink folder
 #define DEBUG 1
 
 #endif //ENVIORMENT_H
