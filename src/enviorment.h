0fe11ed247fa76f958dfcdad4954b3dcb685c549
olakrhoff <ola.kr@hotmail.no>
2021-08-21 11:43:08 +0200

    qws

--- src/enviorment.h
+++
@@ -1,7 +0,0 @@
-#ifndef ENVIORMENT_H
-#define ENVIORMENT_H
-
-#define ENV_PATH ".../shellLink" //This path needs to be upadeted to hold the full path to the shellLink folder
-#define DEBUG 1
-
-#endif //ENVIORMENT_H
